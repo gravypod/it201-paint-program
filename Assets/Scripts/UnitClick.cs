b3e8f20c4a62834567a1399a01ef86ef761b2976
Joshua Katz <gravypod@gravypod.com>
2019-09-13 04:46:00 -0400

    Place spheres in a 3d space.

---
+++ Assets/Scripts/UnitClick.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Object = System.Object;
+
+public class UnitClick : MonoBehaviour
+{
+    public GameObject paintBlobTemplate;
+    const float PAINT_DRAW_DISTANCE = 20.0f;
+
+    // Use this for initialization
+    private void Start()
+    {
+        Debug.Log("Starting UnitClick");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var c = GetComponent<Camera>();
+        var ray = c.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(1))
+            UpdateErase(ray);
+
+        if (Input.GetMouseButtonDown(0))
+            UpdateDraw(ray);
+    }
+
+    private void UpdateErase(Ray ray)
+    {
+        RaycastHit hit;
+        if (!Physics.Raycast(ray, out hit, PAINT_DRAW_DISTANCE + 10))
+            return;
+        Debug.Log("Deleting colleded");
+        Destroy(hit.collider.gameObject);
+    }
+
+    private void UpdateDraw(Ray ray)
+    {
+        Debug.Log("UpdateDraw");
+        RaycastHit hit;
+
+        // Assu
+        var point = Physics.Raycast(ray, out hit, PAINT_DRAW_DISTANCE)
+            ? hit.point
+            : ray.GetPoint(PAINT_DRAW_DISTANCE);
+
+        Instantiate(this.paintBlobTemplate, point, Quaternion.identity);
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# IT201 - Unity Paint Project
+
+This repository contains a "painting" program that is built in the unity3d
+game engine. This program, when run, allows a user to draw with spherical 
+objects. Each sphere is placed in a 3D space and randomly generated. 
+
+# Requirements
+
+1. Unity Version: 2018.2.21f1
