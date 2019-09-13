863ffb0ca682c2f50310953558a0fb98244aa220
Joshua Katz <gravypod@gravypod.com>
2019-09-13 05:29:47 -0400

    Add "Clear" button that deletes all objects when clicked.

---
+++ Assets/Scripts/UnitUi.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitUi : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void OnClearPressed()
+    {
+        Debug.Log("[Clear] - Pressed");
+
+        foreach (var paintObject in GameObject.FindGameObjectsWithTag("PaintObject"))
+        {
+            Destroy(paintObject);
+        }
+    }
+}
