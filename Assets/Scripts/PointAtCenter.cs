79513529449e5f0d2ce5684b5f8068e80ddc92f4
Joshua Katz <gravypod@gravypod.com>
2019-10-25 13:34:42 -0400

    Sun Rise/Set Around Camera/Canvas

---
+++ Assets/Scripts/PointAtCenter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAtCenter : MonoBehaviour
+{
+    public Transform sunTransform;
+    
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var center = new Vector3(320, 0, 0);
+        
+        sunTransform.RotateAround(center, new Vector3(1, 0, 0), 20 * Time.deltaTime);
+        // Look towards the camera
+        sunTransform.LookAt(center);
+    }
+}
